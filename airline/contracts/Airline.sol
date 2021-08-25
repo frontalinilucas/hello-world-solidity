ec564034d33e8b94e4e68bd442952698eba88b87
Lucas Frontalini <lucas.frontalini@mercadolibre.com>
2021-08-24 21:06:43 -0300

    airline contract

---
+++ airline/contracts/Airline.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.24;
+
+import './Ownable.sol';
+
+contract Airline is Ownable {
+
+    struct Customer {
+        uint loyaltyPoint;
+        uint totalFlights;
+    }
+
+    struct Flight {
+        string name;
+        uint price;
+    }
+
+    uint etherPerPoint = 0.5 ether;
+
+    Flight[] public flights;
+    mapping(address => Customer) public customers;
+    mapping (address => Flight[]) public customerFlights;
+    mapping(address => uint) public customerTotalFlights;
+
+    event FlightPurchased(address indexed customer, uint price);
+
+    constructor() public {
+        flights.push(Flight('Tokio', 4 ether));
+        flights.push(Flight('Germany', 1 ether));
+        flights.push(Flight('Madrid', 2 ether));
+    }
+
+    function buyFlight(uint flightIndex) public payable {
+        Flight memory flight = flights[flightIndex];
+        require(msg.value >= flight.price);
+
+        Customer storage customer = customers[msg.sender];
+        customer.loyaltyPoint += 5;
+        customer.totalFlights += 1;
+        customerFlights[msg.sender].push(flight);
+        customerTotalFlights[msg.sender]++;
+
+        emit FlightPurchased(msg.sender, flight.price);
+    }
+
+    function totalFlights() public view returns (uint) {
+        return flights.length;
+    }
+
+    function getFundableEther() public view returns(uint) {
+        return customers[msg.sender].loyaltyPoint * etherPerPoint;
+    }
+
+    function redeemLoyaltyPoints() public {
+        Customer storage customer = customers[msg.sender];
+        uint etherToRefund = customer.loyaltyPoint * etherPerPoint;
+        msg.sender.transfer(etherToRefund);
+        customer.loyaltyPoint = 0;
+    }
+
+    function getAirlineBalance() public onlyOwner view returns(uint) {
+        address airlineAddress = this;
+        return airlineAddress.balance;
+    }
+
+}
