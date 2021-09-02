b9ed510595227dc3fd2338d65ae1b97856423ced
Lucas Frontalini <lucas.frontalini@mercadolibre.com>
2021-09-01 23:10:54 -0300

    Airline finished

--- airline/truffle-config.js
+++ airline/truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const mnemonic = '[YOUR_MNEMONIC]';
+
 module.exports = {
   networks: {
     development: {      
@@ -5,6 +9,12 @@ module.exports = {
       port: 7545,
       network_id: '*',
       gas: 5000000
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, "https://goerli.infura.io/v3/[YOUR_PROJECT_ID]"),
+      network_id: 5,
+      networkCheckTimeout: 100000000000,
+      timeoutBlocks: 500,
     }
   }
 }
